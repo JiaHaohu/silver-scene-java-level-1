90dd0fd6d507cf1408b02eef5e191b8b7cb88f83
jiahao <hujiahao_pa@163.com>
2020-05-31 22:26:47 +0800

    [hujiahao] feat: complete the should_leave_original_array_unchanged test!

--- src/main/java/com/tw/ArrayReverser.java
+++ src/main/java/com/tw/ArrayReverser.java
@@ -1,5 +1,8 @@
 package com.tw;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ArrayReverser {
     /**
      * This method reverse each element of the array and returns a reversed new array.
@@ -11,12 +14,18 @@ public class ArrayReverser {
     public static int[] reverse(int[] array) {
         // TODO: Please implement the method
         // <-start-
+
         if (array == null || array.length == 0 || array.length == 1) {
             return array;
         }
 
+        int[] reverseArray = new int[array.length];
 
-        throw new RuntimeException("Delete me");
+        for (int i = array.length-1; i >= 0; i--) {
+            reverseArray[(array.length-1)-i] = array[i];
+        }
+
+        return reverseArray;
         // --end-->
     }
 
